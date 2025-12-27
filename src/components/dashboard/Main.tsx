141fbe086dd518f442cc1cb78eb1e33c61bc4029
gemy3731 <mohamedgamalz3731@gmail.com>
2025-12-27 21:58:30 +0200

    feat(dashboard): handle delete product

--- src/components/dashboard/Main.tsx
+++ src/components/dashboard/Main.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/Auth.context";
 import type { Product } from "../../types";
-import { getProductsByVendor } from "../../services";
+import { deleteProduct, getProductsByVendor } from "../../services";
 import { ProductsCard } from "../shared";
 import { Loader } from "../ui";
 
@@ -15,12 +15,22 @@ const Main = () => {
             getProductsByVendor(state.user?.id as string).then(setProducts).catch(console.error);
         }
     },[state])
+
+    const handleDeleteProduct = async (id: string) => {
+        try {
+          console.log("Deleting product with id:", id);
+          await deleteProduct(id);
+          // window.location.reload();
+        } catch (error) {
+          console.error("Failed to delete product:", error);
+        }
+      }
     
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {state.loading&&<Loader />}
         {products.map((product) => (
-            <ProductsCard key={product.id} product={product} />
+            <ProductsCard key={product.id} product={product} handleDeleteProduct={handleDeleteProduct} setProducts={setProducts} />
         ))}
     </main>
   )
--- src/components/shared/ProductsCard.tsx
+++ src/components/shared/ProductsCard.tsx
@@ -4,12 +4,25 @@ import { useAuth } from "../../context/Auth.context";
 import { useLocation } from "react-router";
 import { useCart } from "../../hooks";
 
-const ProductsCard = ({ product }: { product: Product }) => {
+interface ProductsCardProps {
+  product: Product;
+  handleDeleteProduct: (id: string) => Promise<void>;
+  setProducts: React.Dispatch<React.SetStateAction<Product[]>>;
+}
+const ProductsCard = ({ product,handleDeleteProduct,setProducts }: ProductsCardProps) => {
   const { addItem } = useCart();
   const { state } = useAuth();
   const pathname = useLocation().pathname;
   const isCustomer = state.isAuth && state.user?.role === "customer";
   const isVendor = state.isAuth && state.user?.role === "vendor";
+
+  const handleDelete = async (productId: string) => {
+    await handleDeleteProduct(productId);
+    setProducts((prevProducts) =>
+      prevProducts.filter((p) => p.id !== productId)
+    );
+  }
+ 
   return (
     <div className="rounded-lg border border-border bg-card text-card-foreground shadow-sm overflow-hidden hover:shadow-lg transition-all group">
       <div className="relative h-48 overflow-hidden bg-muted">
@@ -58,7 +71,7 @@ const ProductsCard = ({ product }: { product: Product }) => {
           <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 flex-1">
             <SquarePen size={16} className="shrink-0 w-4 h-4" /> Edit
           </button>
-          <button className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-destructive text-destructive-foreground hover:bg-destructive/90 h-10 px-4 py-2">
+          <button onClick={()=>{handleDelete(product.id!)}} className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-destructive text-destructive-foreground hover:bg-destructive/90 h-10 px-4 py-2">
             <Trash2 size={16} className="shrink-0 w-4 h-4" />
           </button>
         </div>
