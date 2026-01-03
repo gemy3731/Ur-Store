e80fb5baae198bb16ed72f99a9b54c0c61e1d576
gemy3731 <mohamedgamalz3731@gmail.com>
2026-01-03 18:30:58 +0200

    fix(protectors): add loader to wait auth state

--- src/protections/CustomerProtector.tsx
+++ src/protections/CustomerProtector.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../context/Auth.context";
+import { Loader } from "../components";
 
 interface CustomerProtectorProps {
     children: React.ReactNode;
@@ -7,6 +8,9 @@ interface CustomerProtectorProps {
 
 const CustomerProtector = ({children}: CustomerProtectorProps) => {
     const {state} = useAuth();
+    if(state.loading) {
+        return <Loader />;
+    }
     if(state.isAuth && state.user?.role === 'customer') {
         return children
     }else{
--- src/protections/SetupProtector.tsx
+++ src/protections/SetupProtector.tsx
@@ -1,19 +1,20 @@
 import { Navigate } from "react-router";
 import { useAuth } from "../context/Auth.context"
+import { Loader } from "../components";
 
 interface SetupProtectorProps {
     children: React.ReactNode;
 }
 const SetupProtector = ({children}: SetupProtectorProps) => {
     const {state} = useAuth();
+    if(state.loading){
+        return <Loader />;
+    }
     if(state.isAuth && !state.user?.username){
         return children;
     }else{
         return <Navigate to={'/home'} replace />;
     }
-  return (
-    <div>SetupProtector</div>
-  )
 }
 
 export default SetupProtector
